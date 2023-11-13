760248c169fee7bdea21dd7cde24b6a3dd78cfe9
Nick Kuijpers <kuijpers2nick@gmail.com>
2023-11-13 16:20:43 +0100

    added inference

--- backend/inference.py
+++ backend/inference.py
@@ -10,6 +10,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
+import json
+
+image_transforms = {
+    'test': transforms.Compose([
+        transforms.Resize(size=256),
+        transforms.CenterCrop(size=224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+}
+
+img_dataset = datasets.ImageFolder(
+    root = "../../data/CNN_images/Run1/",
+    transform = image_transforms["test"]
+)  
+
+idx2class = {v: k for k, v in img_dataset.class_to_idx.items()}
+
+# number of classes
+num_classes = 17
+
 
 def predict(model, test_image_name):
     '''
@@ -23,7 +45,7 @@ def predict(model, test_image_name):
     transform = image_transforms['test']
 
     test_image = Image.open(test_image_name)
-    plt.imshow(test_image)
+    # plt.imshow(test_image)
     
     test_image_tensor = transform(test_image)
 
@@ -38,8 +60,30 @@ def predict(model, test_image_name):
         out = model(test_image_tensor)
         ps = torch.exp(out)
         topk, topclass = ps.topk(3, dim=1)
+        predictions = []
         for i in range(3):
-            print("Predcition", i+1, ":", idx_to_class[topclass.cpu().numpy()[0][i]], ", Score: ", topk.cpu().numpy()[0][i])
+            # print("Prediction", i+1, ":", idx2class[topclass.cpu().numpy()[0][i]], ", Score: ", topk.cpu().numpy()[0][i])
+            prediction = {
+                "class": idx2class[topclass.cpu().numpy()[0][i]],
+                "score": float(topk.cpu().numpy()[0][i])
+            }
+            predictions.append(prediction)
+        return json.dumps(predictions)
+        
 
-model = torch.load('trained_model.pt')
-predict(model, 'food-11k-sub/test/Egg/3_22.jpg')
+# model = torch.load('/home/nick-kuijpers/Documents/Railnova/Python/backend/models/trained_model.pt') #GPU only
+model = torch.load('/home/nick-kuijpers/Documents/Railnova/Python/backend/models/trained_model.pt',map_location ='cpu')
+# predict(model, '../../data/CNN_images/Run1/J2/J2_2F3_G12.jpg') #Confondu avec U600
+# predict(model, '../../data/CNN_images/Run1/U600/U600_2F2_G244.jpg') # ok avec U600
+# predict(model, '../../data/CNN_images/Run1/U911U5/U911U5_2F2_G219.jpg') # Confonds encore avec du U600...
+# predict(model, '../../data/CNN_images/Run1/U701/U701_2F2_G156.jpg') # U904U3...
+
+def main():
+    # Load the model
+    model = torch.load('/home/nick-kuijpers/Documents/Railnova/Python/backend/models/trained_model.pt', map_location='cpu')
+
+    # Call the predict function with a test image
+    predict(model, '../../data/CNN_images/Run1/J2/J2_2F3_G12.jpg')
+
+if __name__ == '__main__':
+    main()
--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json 
+import torch
+
+from inference import predict
 
 app = Flask(__name__)
 CORS(app)
@@ -7,14 +11,18 @@ CORS(app)
 @app.route('/analyze', methods=['POST'])
 def analyze_image():
     data = request.get_json()
-    image_path = data.get('path')
+    image_path = data.get('path').get('path')
+    
+    # Find image
+    folder_name = image_path.split('_')[0]
+    base_path = f'../../data/CNN_images/Run1/{folder_name}/'
+    real_image_path = base_path + image_path
 
-    # Perform image analysis logic here
-    # ...
-
-    # Example response data
-    print(image_path)
-    analysis_results = {'result': 'Analysis complete', 'image_path': image_path}
+    # Load model and execute inference
+    model = torch.load('/home/nick-kuijpers/Documents/Railnova/Python/backend/models/trained_model.pt', map_location='cpu')
+    predictions = predict(model, real_image_path)
+    # Return results
+    analysis_results = {'result': 'Analysis complete', 'image_path': image_path, 'predictions': predictions}
     return jsonify(analysis_results)
 
 if __name__ == '__main__':
